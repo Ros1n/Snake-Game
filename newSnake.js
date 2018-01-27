fc2867520d41f61a83100fbfcc6c1e0c95322057
xin <xluo@protonmail.com>
2018-01-27 14:36:11 +1300

    second commit

--- newSnake.js
+++ newSnake.js
@@ -1,89 +1,86 @@
 (function() {
-	// initialise necessary global variables
-	let myCanvas = document.getElementById('mc'); //please read through introduction of JS-canvas
-	let ctx = myCanvas.getContext('2d'); //create cantext instance
-	let size = 500, sz = snakeSize = 10, score = 0, snake; //2x resolution for retina screens look
-	//let myCanvas.height = myCanvas.width = size * 2, myCanvas.style.width = myCanvas.style.height = size + 'px';
-	let direction = newDirection = 1, end = false, length = 4;
-	ctx.scale(2, 2); //scale canvas for retina screens
+  // initialise necessary global variables
+  let myCanvas = document.getElementById('mc'); //please read through introduction of JS-canvas
+  let ctx = myCanvas.getContext('2d');  //create cantext instance
+  let size = 500; // size of the play-field in pixels
+  let GRID_size = size / 50;
+  let direction = newDirection = 1; // -2: up, 2: down, -1: left, 1: right
+  let len = 5, candy = null, snake = [{ x: size / 2, y: size / 2}]; // Snake starts in the center
 
-	//put basic elements inside this module
-	function bodySnake(x, y){
-		ctx.fillStyle = 'green'; //fill color
-		//fill a single rectangle with sz x sz and start at (x*sz, y*sz)
-		ctx.fillRect(x*sz, y*sz, sz, sz);
-		ctx.strokeStyle = 'darkgreen'; // border of the grid
-		ctx.strokeRect(x*sz, y*sz, sz, sz);
-	}
+  function fillCandy(x, y) {
+    ctx.fillStyle = 'yellow';  //single grid of cnady
+    ctx.fillRect(x, y, GRID_size, GRID_size);
+  }
 
-	function candy(x, y){
-		ctx.fillStyle = 'yellow'; //border of candy
-		ctx.fillRect(x*sz, y*sz, sz, sz);
-		ctx.fillStyle = '#b58900'; // single grid of cnady
-		ctx.fillRect(x*sz+1, y*sz+1, sz-2, sz-2);
-	}
+  //position of candy --> must not repeat with snake
+  function createCandy() {
+    candy = {
+      x: Math.floor(Math.random() * size / GRID_size) * GRID_size,
+      y: Math.floor(Math.random() * size / GRID_size) * GRID_size
+    }
+    if (snake.includes(candy)) {
+      return createCandy();
+    } else {
+      return candy;
+    }
+  }
 
-	//position of candy --> must not repeat with snake
-	function createCandy(){
-		candy = {
-			x: Math.floor((Math.random()*(size/sz))+1), y: Math.floor((Math.random()*(size/sz))+1)
-		}
-		if (snake.includes(candy)){
-			return createCandy();
-		}else{
-			return candy;
-		}
-	}
+  //main function
+  function Game() {
+    ctx.fillStyle = '#002b36';
+    ctx.fillRect(0, 0, size, size); // initialize/reset space/area
+    let newHead = {x: snake[0].x, y: snake[0].y};
 
-	//main function
-	function game(){
-		//initialize space/area
-		ctx.fillStyle = '#002b36';
-		ctx.fillRect(0, 0, size, size); //draw the space/area of game
-		ctx.strokeStyle = 'black';
-		ctx.strokeRect(0, 0, size, size); //the border of area
+    // Only change directon if the new direction is a different axis
+    if (Math.abs(direction) !== Math.abs(newDirection)) {
+      direction = newDirection;
+    }
+    let axis = Math.abs(direction) === 1 ? 'x' : 'y'; // 1, -1 are X; 2, -2 are Y
+    if (direction < 0) {
+      newHead[axis] -= GRID_size; // Move left or down
+    } else {
+      newHead[axis] += GRID_size; // Move right or up
+    }
 
-		//initialize snake, structure of snake
-		snake = [{x: size/2, y: size/2}];
-		for (let i = length; i >= 0; i--){
-			snake.push( {x:i, y:0} ); //push 5 grids inside array with given coordinate
-			bodySnake(i, 0);
-		}
-		let newHead = {x: snake[0].x, y: snake[0].y};
+    // Did we eat a candy? Detect if our head is in the same cell as the candy
+    if (candy && candy.x === newHead.x && candy.y === newHead.y) {
+      candy = null;
+      len+=10;
+    }
 
-		//initialize scoreboard 
-		let score_text = "Score: " + score;
-		ctx.fillStyle = 'blue';
-		ctx.font = '40px serif';
-		ctx.fillText(score_text, size-5, size-5); //write text at position(w-5, h-5)		
+    snake.unshift(newHead); // Add the new head to the front
+    snake = snake.slice(0, len); // Enforce the snake's max length
 
-		while (!candy) candy = createCandy();
 
-		if (Math.abs(direction) !== Math.abs(newDirection)){
-			direction = newDirection;
-		}
-		let axis = Math.abs(direction) === 1 ? 'x' : 'y';
-		if (direction < 0){
-			newHead[axis] -= sz;
-		}else{
-			newHead[axis] += sz;
-		}
+    // Detect wall collisions
+    if (newHead.x < 0) newHead.x = size - 1;
+    if (newHead.x >= size) newHead.x = 0;
+    if (newHead.y < 0) newHead.y = size - 1;
+    if (newHead.y >= size) newHead.y = 0;
 
-		if (candy && candy.x === newHead.x && candy.y === newHead.y){
-			candy = null;
-			length += sz;
-		}
+    //initialize snake, structure of snake
+    ctx.fillStyle = '#268bd2';
+    for (let i = 0; i < snake.length; i++) {
+      ctx.fillRect(snake[i].x, snake[i].y, GRID_size, GRID_size); // Paint the snake
+      if (snake[i].x == newHead.x && snake[i].y == newHead.y) {
+        len = 5;
+      }
+    }
+    while (snake.length > len){
+      snake.shift();
+    }
 
-		if (newHead.x < 0) newHead.x = size-1;
-		if (newHead.x >= size) newHead.x = 0;
-		if (newHead.y < 0) newHead.y = size-1;
-		if (newHead.y >= size) newHead.y = 0;
-	}
+    // Place a candy (not on the snake) if needed
+    while (!candy) {
+      candy = createCandy();
+    }
+    fillCandy(candy.x, candy.y)
+  }
 
-	window.onload = function() {
-		setInterval(game, 100);
-		window.onkeydown = function(e) {
-			newDirection = {37:-1, 38:-2, 39:1, 40:2}[e.keyCode] || newDirection;
-		};
-	};
+  window.onload = function() {
+    setInterval(Game, 100);
+    window.onkeydown = function(e) {
+      newDirection = { 37: -1, 38: -2, 39: 1, 40: 2 }[e.keyCode] || newDirection; 
+    };
+  };
 })();
